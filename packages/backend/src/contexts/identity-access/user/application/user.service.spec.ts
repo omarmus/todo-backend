fcbdd6610753ee78b4adadf63ed01e35959151b3
Omar Gutiérrez Condori <omargc.mus@gmail.com>
2026-09-03 22:05:35 -0400

    fix: update user service tests for NotificationPort and create dto

--- packages/backend/src/contexts/identity-access/user/application/user.service.spec.ts
+++ packages/backend/src/contexts/identity-access/user/application/user.service.spec.ts
@@ -2,10 +2,12 @@ import { Test, TestingModule } from '@nestjs/testing';
 import { User } from '../domain/user.entity';
 import { UserRepository } from '../domain/user.repository';
 import { UserService } from './user.service';
+import { NotificationPort } from 'src/contexts/tasks/todo/domain/notification.port';
 
 describe('UserService', () => {
   let service: UserService;
   let userRepository: jest.Mocked<UserRepository>;
+  let notificationPort: jest.Mocked<NotificationPort>;
 
   const mockUser = new User(
     '000001',
@@ -29,11 +31,18 @@ describe('UserService', () => {
             findById: jest.fn(),
           },
         },
+        {
+          provide: NotificationPort,
+          useValue: {
+            send: jest.fn(),
+          },
+        },
       ],
     }).compile();
 
     service = module.get<UserService>(UserService);
     userRepository = module.get(UserRepository);
+    notificationPort = module.get(NotificationPort);
   });
 
   it('Debería estar definido', () => {
@@ -55,26 +64,32 @@ describe('UserService', () => {
       userRepository.findByEmail.mockResolvedValue(null);
       userRepository.create.mockResolvedValue(mockUser);
 
-      const result = await service.create({
-        name: mockUser.name,
-        email: mockUser.email,
-        password: 'password123',
-      });
+      const result = await service.create(
+        {
+          name: mockUser.name,
+          email: mockUser.email,
+          password: 'password123',
+        },
+        'caller-id',
+      );
 
       expect(result).not.toHaveProperty('password');
-
       expect(result.email).toBe(mockUser.email);
+      expect(notificationPort.send).toHaveBeenCalled();
     });
 
     it('Email ya existe, debería lanzar un error', async () => {
       userRepository.findByEmail.mockResolvedValue(mockUser);
 
       await expect(
-        service.create({
-          name: mockUser.name,
-          email: mockUser.email,
-          password: 'password123',
-        })
+        service.create(
+          {
+            name: mockUser.name,
+            email: mockUser.email,
+            password: 'password123',
+          },
+          'caller-id',
+        ),
       ).rejects.toThrow('User with this email already exists');
     });
   });
